849f53bebc7b22552b02370481f61cad0af07c85
NemoIII <pablo.iii@outlook.com>
2018-10-30 14:54:16 -0300

    os 'if' estão dando o problema de TypeError!!

--- oi_seven/user/views.py
+++ oi_seven/user/views.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, Flask
 from user.forms import EnviarForm
 from user.models import Enviar
 
 app_page = Blueprint('app_page', __name__)
 user_page = Blueprint('user_page', __name__)
 
+app = Flask(__name__)
 
 # @user_page.route('/index', methods=['POST'])
 @app_page.route('/')
@@ -17,13 +18,13 @@ def enviarForm():
 	special_chars = ['!', '#', '@', ';', ':', ',', '*', '&', '%', '$']
 	letra = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'w', 'x', 'y', 'z', 'll', 'ç']
 	
-	if request.method == 'POST' and form.validate():
+	if request.method == 'POST':
 		envio = Enviar(
 			name=form.name.data.lower(),
 			phone=form.phone.data,
 			mobile=form.mobile.data
 			)
-		if form.name.data.contains(any(number)) or form.name.data.contains(any(special_chars)):
+		''' if form.name.data.contains(any(number)) or form.name.data.contains(any(special_chars)):
 			error = "O nome deve conter apenas letras."
 		if form.phone.data.contains(any(letra)) or form.mobile.data.contains(any(letra)):
 			error = "Este campo deve conter apenas números."
@@ -31,8 +32,12 @@ def enviarForm():
 			envio.save()
 			message = f"Obrigado por entrar em contato, {form.name.data.lower()}."
 			return message
-		redirect(url_for('enviarForm'))
+		redirect(url_for('enviarForm'))'''
 	return render_template("enviar_form.html", form=form, error=error, message=message)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
 
 
 # def index():
